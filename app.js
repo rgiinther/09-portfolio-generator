fc754c3e837a585b1e6e6eb4bb3c7851d19078a3
rgiinther <r.giin95@gmail.com>
2022-05-01 22:13:51 -0500

    9.2 modulalized files

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello Node!');
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+const generatePage = require('./src/page-template.js');
